3b2430caab4bc06e64bb7c4395f7619471846aae
Alberto Duarte <betodute@gmail.com>
2019-04-10 17:52:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue (object, key, value) {
-  object.assign({}, object, {[key] : value});
-  return object;
+ return object.assign({}, object, {[key] : value});
 }
