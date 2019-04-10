2b9837bd9eb604f24bdd4ffc93c2e402ec24b955
Alberto Duarte <betodute@gmail.com>
2019-04-10 17:43:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes = {spicybeans:"red beans"};
 
 function updateObjectWithKeyAndValue (obj, key, value) {
-  object.assign({}, obj, {key : value});
+  recipes.assign({}, obj, {key : value});
   return obj;
 }
